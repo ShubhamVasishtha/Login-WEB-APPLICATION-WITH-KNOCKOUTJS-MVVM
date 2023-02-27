4a86d3d33ec3062d8ed7b7f1ec077603bce25230
Shubham <Shubham@SHUBHAM-PC>
2023-02-27 20:06:29 +0530

    Add project files.

---
+++ Knockout Login/Login.js
@@ -0,0 +1,33 @@
+﻿var Log = (function () {
+
+    var _uname = ko.observable("");
+    var _pass = ko.observable("");
+    
+    var _click = function () {
+
+        $.ajax({
+            url: "/Login_Controller.ashx?method=login",
+            type: "get",
+            async: false,
+            cache: false,
+            dataType: "json",
+            data: {username: _uname() ,password:_pass()},
+            success: function (result) {
+                alert(result);              
+            },
+            error: function (result) {
+                alert(result);
+                return ([]);
+            }
+        });
+    }
+
+    return {
+        username: _uname,
+        password: _pass,
+        submit: _click
+    };
+});
+ko.applyBindings(Log);
+
+
