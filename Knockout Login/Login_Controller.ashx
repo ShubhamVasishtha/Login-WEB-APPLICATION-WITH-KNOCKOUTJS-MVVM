05a5e4097f26eb18d91dbf1701d826eb979bb7d4
Shubhamsharma3k@gmail.com <shubhamsharma3k@gmail.com>
2024-11-09 23:07:49 +0530

    First time check-in

---
+++ Knockout Login/Login_Controller.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="Login_Controller.ashx.cs" Class="Knockout_Login.Loginclass" %>
---
+++ Knockout Login/Login_Controller.ashx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Westwind.Web;
+using System.Data.SqlClient;
+
+namespace Knockout_Login
+{
+
+    public class Loginclass : CallbackHandler
+    {
+
+        [CallbackMethod]
+        public string login(string username, string password)
+        {
+            string conn = "Server=(localdb)\\MSSQLLocalDB;Database=LoginForm;Trusted_Connection=True;";
+            SqlConnection con = new SqlConnection(conn);
+            object result = "";
+            SqlCommand cmd = new SqlCommand();
+            try
+            {
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = string.Format("SELECT CASE WHEN EXISTS (SELECT username from login where username='{0}' and password='{1}') THEN 'User Authenticated' Else 'Invalid User or Password' End", username, password);
+               result = cmd.ExecuteScalar();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+            return result.ToString();
+
+        }
+    }
+}
